559464eb75c432235de6373640c1afd5cb827d17
schmidtu <schmidtu@PC-352.heuch.local>
2018-04-20 07:51:24 +0200

    Neuerstellung

---
+++ ManagerDatenpflegeWerkzeugstamm/src/managerdatenpflegewerkzeugstamm/ManagerDatenpflegeWerkzeugstamm.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managerdatenpflegewerkzeugstamm;
+import GUI.ManagerFrame;
+
+/**
+ *
+ * @author schmidtu
+ */
+public class ManagerDatenpflegeWerkzeugstamm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ManagerFrame myManager = new ManagerFrame();
+        myManager.setTitle("Manager Werkzeugstamm");
+        myManager.setVisible(true);
+    }
+    
+}
