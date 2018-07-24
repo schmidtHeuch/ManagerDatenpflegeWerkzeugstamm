c17e2f34a72fa1f0e13560a0358aeede42f3e660
schmidtu <schmidtu@PC-352.heuch.local>
2018-07-24 15:02:21 +0200

    Fehlerbehebung

--- ManagerDatenpflegeWerkzeugstamm/src/GUI/Frame_DataMaintenance_Schnitt.java
+++ ManagerDatenpflegeWerkzeugstamm/src/GUI/Frame_DataMaintenance_Schnitt.java
@@ -36,6 +36,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     public Frame_DataMaintenance_Schnitt() {
         InstanceCount++;
         this.Old_Bezeichnung = "";
+        this.Old_Beschreibung = "";
         this.Old_Neu_nach_Produktion = false;
         this.Old_Anlagedatum = "";
         this.Old_Änderungsdatum = "";
@@ -60,6 +61,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     DB_ConnectionManager MY_DBCM;
     String Old_Key;
     String Old_Bezeichnung;
+    String Old_Beschreibung;
     boolean Old_Neu_nach_Produktion;
     String Old_Anlagedatum;
     String Old_Änderungsdatum;
@@ -130,9 +132,12 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
         jPanel_editLabels = new javax.swing.JPanel();
         lbl_key = new javax.swing.JLabel();
         lbl_Bezeichnung = new javax.swing.JLabel();
+        lbl_Beschreibung = new javax.swing.JLabel();
         jPanel_editTextFields = new javax.swing.JPanel();
         jTextField_key = new javax.swing.JTextField();
         jFormattedTextField_Bezeichnung = new javax.swing.JFormattedTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea_Beschreibung = new javax.swing.JTextArea();
         jCheckBox_Neu_nach_Produktion = new javax.swing.JCheckBox();
         jPanel_editButtons = new javax.swing.JPanel();
         btn_new = new javax.swing.JButton();
@@ -185,14 +190,14 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
 
             },
             new String [] {
-                "KWS (Key)", "Bezeichnung", "Neu nach Produktion?", "Anlagedatum", "Änderungsdatum", "Benutzer"
+                "KWS (Key)", "Bezeichnung", "Beschreibung", "Neu nach Produktion?", "Anlagedatum", "Änderungsdatum", "Benutzer"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -211,10 +216,11 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
         if (jTable_dbData.getColumnModel().getColumnCount() > 0) {
             jTable_dbData.getColumnModel().getColumn(0).setPreferredWidth(120);
             jTable_dbData.getColumnModel().getColumn(1).setPreferredWidth(200);
-            jTable_dbData.getColumnModel().getColumn(2).setPreferredWidth(150);
+            jTable_dbData.getColumnModel().getColumn(2).setPreferredWidth(200);
             jTable_dbData.getColumnModel().getColumn(3).setPreferredWidth(150);
             jTable_dbData.getColumnModel().getColumn(4).setPreferredWidth(150);
-            jTable_dbData.getColumnModel().getColumn(5).setPreferredWidth(100);
+            jTable_dbData.getColumnModel().getColumn(5).setPreferredWidth(150);
+            jTable_dbData.getColumnModel().getColumn(6).setPreferredWidth(100);
         }
 
         lbl_search1.setText("Suchen");
@@ -270,7 +276,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
                             .addComponent(btn_getCurrentDBData)))
                     .addComponent(lbl_rowCount, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane_dbData, javax.swing.GroupLayout.DEFAULT_SIZE, 265, Short.MAX_VALUE)
+                .addComponent(jScrollPane_dbData, javax.swing.GroupLayout.DEFAULT_SIZE, 258, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -289,6 +295,10 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
         lbl_Bezeichnung.setPreferredSize(new java.awt.Dimension(200, 14));
         jPanel_editLabels.add(lbl_Bezeichnung);
 
+        lbl_Beschreibung.setText("Beschreibung");
+        lbl_Beschreibung.setPreferredSize(new java.awt.Dimension(200, 14));
+        jPanel_editLabels.add(lbl_Beschreibung);
+
         jPanel_editTextFields.setOpaque(false);
         jPanel_editTextFields.setPreferredSize(new java.awt.Dimension(714, 23));
         java.awt.FlowLayout flowLayout1 = new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 0, 0);
@@ -302,11 +312,23 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
         jTextField_key.setPreferredSize(new java.awt.Dimension(120, 20));
         jPanel_editTextFields.add(jTextField_key);
 
+        jFormattedTextField_Bezeichnung.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 153)), javax.swing.BorderFactory.createEmptyBorder(1, 2, 1, 2)));
         jFormattedTextField_Bezeichnung.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         jFormattedTextField_Bezeichnung.setDisabledTextColor(new java.awt.Color(102, 102, 102));
         jFormattedTextField_Bezeichnung.setEnabled(false);
         jFormattedTextField_Bezeichnung.setPreferredSize(new java.awt.Dimension(200, 20));
         jPanel_editTextFields.add(jFormattedTextField_Bezeichnung);
+
+        jTextArea_Beschreibung.setColumns(18);
+        jTextArea_Beschreibung.setLineWrap(true);
+        jTextArea_Beschreibung.setRows(3);
+        jTextArea_Beschreibung.setWrapStyleWord(true);
+        jTextArea_Beschreibung.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 153)), javax.swing.BorderFactory.createEmptyBorder(1, 2, 1, 2)));
+        jTextArea_Beschreibung.setDisabledTextColor(new java.awt.Color(102, 102, 102));
+        jTextArea_Beschreibung.setEnabled(false);
+        jScrollPane1.setViewportView(jTextArea_Beschreibung);
+
+        jPanel_editTextFields.add(jScrollPane1);
 
         jCheckBox_Neu_nach_Produktion.setText("Neu nach Produktion?");
         jCheckBox_Neu_nach_Produktion.setEnabled(false);
@@ -453,7 +475,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_editDataLayout.createSequentialGroup()
                 .addComponent(jPanel_editLabels, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel_editTextFields, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel_editTextFields, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel_editDataLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel_editButtons, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -525,7 +547,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
                 .addGroup(jPanel_baseLayout.createSequentialGroup()
                     .addContainerGap()
                     .addComponent(jPanel_table, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGap(216, 216, 216)))
+                    .addGap(260, 260, 260)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -583,7 +605,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
                             myDataSet = "nein";
                         }
 
-                        if (myDataSet != null && i == 4 || myDataSet != null && i == 5) {
+                        if (myDataSet != null && i == 5 || myDataSet != null && i == 6) {
                             Timestamp ts = Timestamp.valueOf(myDataSet);
                             myDataSet = myFormat.format(ts);
                         }
@@ -718,6 +740,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
                     return;
                 }
                 if (DataSet_Mode.equals("edit") && !Old_Bezeichnung.equals(jFormattedTextField_Bezeichnung.getText().trim())
+                    || !Old_Beschreibung.equals(jTextArea_Beschreibung.getText().trim())
                     || Old_Neu_nach_Produktion != jCheckBox_Neu_nach_Produktion.isSelected()) {
                     do_updateDataSet_inDB();
                 }
@@ -849,20 +872,26 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
                 
                 if (myTableModel.getValueAt(myRow, 2) != null) {
                 tempString = myTableModel.getValueAt(myRow, 2).toString();
+                jTextArea_Beschreibung.setText(tempString);
+                }
+                else {jTextArea_Beschreibung.setText("");}
+                 
+                if (myTableModel.getValueAt(myRow, 3) != null) {
+                tempString = myTableModel.getValueAt(myRow, 3).toString();
                 if (tempString.equals("ja")) {
                     jCheckBox_Neu_nach_Produktion.setSelected(true);
                     }else {jCheckBox_Neu_nach_Produktion.setSelected(false);}
                 }
-                if (myTableModel.getValueAt(myRow, 3) != null) {
-                    jTextField_Anlagedatum.setText(myTableModel.getValueAt(myRow, 3).toString().trim());
+                if (myTableModel.getValueAt(myRow, 4) != null) {
+                    jTextField_Anlagedatum.setText(myTableModel.getValueAt(myRow, 4).toString().trim());
                 }
                 else {jTextField_Anlagedatum.setText("");}
-                if (myTableModel.getValueAt(myRow, 4) != null) {
-                   jTextField_Änderungsdatum.setText(myTableModel.getValueAt(myRow, 4).toString().trim());
+                if (myTableModel.getValueAt(myRow, 5) != null) {
+                   jTextField_Änderungsdatum.setText(myTableModel.getValueAt(myRow, 5).toString().trim());
                 }
                 else {jTextField_Änderungsdatum.setText("");}
-                if (myTableModel.getValueAt(myRow, 5) != null) {
-                    jTextField_Benutzer.setText(myTableModel.getValueAt(myRow, 5).toString().trim()); 
+                if (myTableModel.getValueAt(myRow, 6) != null) {
+                    jTextField_Benutzer.setText(myTableModel.getValueAt(myRow, 6).toString().trim()); 
                 }
                 else {jTextField_Benutzer.setText("");}                
                 
@@ -880,6 +909,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     private void set_textFieldsEmpty() {
         jTextField_key.setText("");
         jFormattedTextField_Bezeichnung.setText("");
+        jTextArea_Beschreibung.setText("");
         jCheckBox_Neu_nach_Produktion.setSelected(false);
         jTextField_Anlagedatum.setText("");
         jTextField_Änderungsdatum.setText("");
@@ -888,6 +918,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     private void set_oldValues() {        
         Old_Key = jTextField_key.getText().trim();
         Old_Bezeichnung = jFormattedTextField_Bezeichnung.getText().trim();
+        Old_Beschreibung = jTextArea_Beschreibung.getText().trim();
         Old_Neu_nach_Produktion = jCheckBox_Neu_nach_Produktion.isSelected();
         Old_Anlagedatum = jTextField_Anlagedatum.getText();
         Old_Änderungsdatum = jTextField_Änderungsdatum.getText();
@@ -896,6 +927,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     private void get_oldValues() {         
         jTextField_key.setText(Old_Key);
         jFormattedTextField_Bezeichnung.setText(Old_Bezeichnung);
+        jTextArea_Beschreibung.setText(Old_Beschreibung);
         jCheckBox_Neu_nach_Produktion.setSelected(Old_Neu_nach_Produktion);
         jTextField_Anlagedatum.setText(Old_Anlagedatum);
         jTextField_Änderungsdatum.setText(Old_Änderungsdatum);
@@ -908,11 +940,13 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
         jTextField_key.setEnabled(aBoolean);
         
         jFormattedTextField_Bezeichnung.setEnabled(true);
+        jTextArea_Beschreibung.setEnabled(true);
         jCheckBox_Neu_nach_Produktion.setEnabled(true);
     }
     private void set_textFieldsDisabled() {        
         jTextField_key.setEnabled(false);
         jFormattedTextField_Bezeichnung.setEnabled(false);
+        jTextArea_Beschreibung.setEnabled(false);
         jCheckBox_Neu_nach_Produktion.setEnabled(false);
     }
     private void do_insertDataSet_intoDB() {        
@@ -934,9 +968,10 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
             String result = myFormat.format(Anlagedatum);
             Benutzer = System.getProperty("user.name"); 
             Statement myStatement = myConnection.createStatement();
-            myStatement.executeUpdate("INSERT INTO DiafBDE.dbo.T_Schnitt (pKey_KWS, Bezeichnung, Neu_nach_Produktion, Anlagedatum, Benutzer)" 
+            myStatement.executeUpdate("INSERT INTO DiafBDE.dbo.T_Schnitt (pKey_KWS, Bezeichnung, Beschreibung, Neu_nach_Produktion, Anlagedatum, Benutzer)" 
                     + "VALUES ('" + jTextField_key.getText().trim() + "', '" 
                     + jFormattedTextField_Bezeichnung.getText().trim() + "', '" 
+                    + jTextArea_Beschreibung.getText().trim() + "', '" 
                     + myBit + "', '" 
                     + result + "', '" 
                     + Benutzer +"')");              
@@ -975,6 +1010,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
 //            System.out.println(jTextArea_Beschreibung.getText().trim());
             Statement myStatement = myConnection.createStatement();
             myStatement.executeUpdate("UPDATE DiafBDE.dbo.T_Schnitt SET Bezeichnung = '" + jFormattedTextField_Bezeichnung.getText().trim() +
+                    "', Beschreibung = '" + jTextArea_Beschreibung.getText().trim() +
                     "', Neu_nach_Produktion = '" + myBit +
                     "', Änderungsdatum = '" + result +
                     "', Benutzer = '" + Benutzer +
@@ -1090,8 +1126,10 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel_editTextFields;
     private javax.swing.JPanel jPanel_footer;
     private javax.swing.JPanel jPanel_table;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane_dbData;
     private javax.swing.JTable jTable_dbData;
+    private javax.swing.JTextArea jTextArea_Beschreibung;
     private javax.swing.JTextField jTextField_Anlagedatum;
     private javax.swing.JTextField jTextField_Benutzer;
     private javax.swing.JTextField jTextField_key;
@@ -1099,6 +1137,7 @@ public class Frame_DataMaintenance_Schnitt extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField_Änderungsdatum;
     private javax.swing.JLabel lbl_Anlagedatum;
     private javax.swing.JLabel lbl_Benutzer;
+    private javax.swing.JLabel lbl_Beschreibung;
     private javax.swing.JLabel lbl_Bezeichnung;
     private javax.swing.JLabel lbl_key;
     private javax.swing.JLabel lbl_rowCount;
