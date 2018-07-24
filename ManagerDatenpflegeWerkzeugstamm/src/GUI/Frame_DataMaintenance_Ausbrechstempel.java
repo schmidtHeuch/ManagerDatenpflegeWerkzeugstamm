6328302aaf97b37a95c95fdba630d0b0eaa54386
schmidtu <schmidtu@PC-352.heuch.local>
2018-07-24 15:00:35 +0200

    Fehlerbehebung

--- ManagerDatenpflegeWerkzeugstamm/src/GUI/Frame_DataMaintenance_Ausbrechstempel.java
+++ ManagerDatenpflegeWerkzeugstamm/src/GUI/Frame_DataMaintenance_Ausbrechstempel.java
@@ -36,6 +36,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     public Frame_DataMaintenance_Ausbrechstempel() {
         InstanceCount++;
         this.Old_Bezeichnung = "";
+        this.Old_Beschreibung = "";
         this.Old_Anlagedatum = "";
         this.Old_Änderungsdatum = "";
         this.Old_Benutzer = "";
@@ -58,6 +59,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     DB_ConnectionManager MY_DBCM;
     String Old_Key;
     String Old_Bezeichnung;
+    String Old_Beschreibung;
     String Old_Anlagedatum;
     String Old_Änderungsdatum;
     String Old_Benutzer;
@@ -127,9 +129,12 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
         jPanel_editLabels = new javax.swing.JPanel();
         lbl_key = new javax.swing.JLabel();
         lbl_Bezeichnung = new javax.swing.JLabel();
+        lbl_Beschreibung = new javax.swing.JLabel();
         jPanel_editTextFields = new javax.swing.JPanel();
         jTextField_key = new javax.swing.JTextField();
         jFormattedTextField_Bezeichnung = new javax.swing.JFormattedTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea_Beschreibung = new javax.swing.JTextArea();
         jPanel_editButtons = new javax.swing.JPanel();
         btn_new = new javax.swing.JButton();
         btn_edit = new javax.swing.JButton();
@@ -192,14 +197,14 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
 
             },
             new String [] {
-                "ASB (Key)", "Bezeichnung", "Anlagedatum", "Änderungsdatum", "Benutzer"
+                "ASB (Key)", "Bezeichnung", "Beschreibung", "Anlagedatum", "Änderungsdatum", "Benutzer"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false
+                false, false, true, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -220,12 +225,13 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
             jTable_dbData.getColumnModel().getColumn(0).setPreferredWidth(127);
             jTable_dbData.getColumnModel().getColumn(1).setMinWidth(200);
             jTable_dbData.getColumnModel().getColumn(1).setPreferredWidth(200);
-            jTable_dbData.getColumnModel().getColumn(2).setMinWidth(150);
-            jTable_dbData.getColumnModel().getColumn(2).setPreferredWidth(150);
+            jTable_dbData.getColumnModel().getColumn(2).setPreferredWidth(200);
             jTable_dbData.getColumnModel().getColumn(3).setMinWidth(150);
             jTable_dbData.getColumnModel().getColumn(3).setPreferredWidth(150);
-            jTable_dbData.getColumnModel().getColumn(4).setMinWidth(100);
-            jTable_dbData.getColumnModel().getColumn(4).setPreferredWidth(100);
+            jTable_dbData.getColumnModel().getColumn(4).setMinWidth(150);
+            jTable_dbData.getColumnModel().getColumn(4).setPreferredWidth(150);
+            jTable_dbData.getColumnModel().getColumn(5).setMinWidth(100);
+            jTable_dbData.getColumnModel().getColumn(5).setPreferredWidth(100);
         }
 
         lbl_rowCount.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
@@ -274,7 +280,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
                             .addComponent(jTextField_searchValue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addComponent(lbl_rowCount, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane_dbData, javax.swing.GroupLayout.DEFAULT_SIZE, 367, Short.MAX_VALUE)
+                .addComponent(jScrollPane_dbData, javax.swing.GroupLayout.DEFAULT_SIZE, 327, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -295,10 +301,16 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
         lbl_Bezeichnung.setPreferredSize(new java.awt.Dimension(200, 14));
         jPanel_editLabels.add(lbl_Bezeichnung);
 
+        lbl_Beschreibung.setText("Beschreibung");
+        lbl_Beschreibung.setPreferredSize(new java.awt.Dimension(200, 14));
+        jPanel_editLabels.add(lbl_Beschreibung);
+
         jPanel_editTextFields.setBackground(new java.awt.Color(204, 255, 255));
         jPanel_editTextFields.setOpaque(false);
         jPanel_editTextFields.setPreferredSize(new java.awt.Dimension(801, 22));
-        jPanel_editTextFields.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 0, 0));
+        java.awt.FlowLayout flowLayout1 = new java.awt.FlowLayout(java.awt.FlowLayout.LEFT, 0, 0);
+        flowLayout1.setAlignOnBaseline(true);
+        jPanel_editTextFields.setLayout(flowLayout1);
 
         jTextField_key.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jTextField_key.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
@@ -307,11 +319,23 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
         jTextField_key.setPreferredSize(new java.awt.Dimension(120, 20));
         jPanel_editTextFields.add(jTextField_key);
 
+        jFormattedTextField_Bezeichnung.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 153)), javax.swing.BorderFactory.createEmptyBorder(1, 2, 1, 2)));
         jFormattedTextField_Bezeichnung.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         jFormattedTextField_Bezeichnung.setDisabledTextColor(new java.awt.Color(102, 102, 102));
         jFormattedTextField_Bezeichnung.setEnabled(false);
         jFormattedTextField_Bezeichnung.setPreferredSize(new java.awt.Dimension(200, 20));
         jPanel_editTextFields.add(jFormattedTextField_Bezeichnung);
+
+        jTextArea_Beschreibung.setColumns(18);
+        jTextArea_Beschreibung.setLineWrap(true);
+        jTextArea_Beschreibung.setRows(3);
+        jTextArea_Beschreibung.setWrapStyleWord(true);
+        jTextArea_Beschreibung.setBorder(javax.swing.BorderFactory.createCompoundBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(153, 153, 153)), javax.swing.BorderFactory.createEmptyBorder(1, 2, 1, 2)));
+        jTextArea_Beschreibung.setDisabledTextColor(new java.awt.Color(102, 102, 102));
+        jTextArea_Beschreibung.setEnabled(false);
+        jScrollPane1.setViewportView(jTextArea_Beschreibung);
+
+        jPanel_editTextFields.add(jScrollPane1);
 
         jPanel_editButtons.setBackground(new java.awt.Color(204, 255, 255));
         jPanel_editButtons.setOpaque(false);
@@ -438,15 +462,19 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
                 .addComponent(jPanel_editButtons, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel_editDataLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lbl_Benutzer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbl_Anlagedatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lbl_Änderungsdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel_editDataLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField_Anlagedatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField_Änderungsdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField_Benutzer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                    .addGroup(jPanel_editDataLayout.createSequentialGroup()
+                        .addComponent(lbl_Benutzer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField_Benutzer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel_editDataLayout.createSequentialGroup()
+                        .addComponent(lbl_Anlagedatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField_Anlagedatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel_editDataLayout.createSequentialGroup()
+                        .addComponent(lbl_Änderungsdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField_Änderungsdatum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(20, Short.MAX_VALUE))
             .addComponent(jPanel_editLabels, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
         );
         jPanel_editDataLayout.setVerticalGroup(
@@ -454,7 +482,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_editDataLayout.createSequentialGroup()
                 .addComponent(jPanel_editLabels, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(5, 5, 5)
-                .addComponent(jPanel_editTextFields, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel_editTextFields, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel_editDataLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_editDataLayout.createSequentialGroup()
@@ -570,10 +598,10 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
                 for (int i = 1; i <= TableColumns; i++) {
                           
                     String myDataSet = myResultSet.getString(i);
-                    if (myDataSet != null && i == 3 || myDataSet != null && i == 4) {
-                        Timestamp ts = Timestamp.valueOf(myDataSet);
-                        myDataSet = myFormat.format(ts);
-                    }
+                        if (myDataSet != null && i == 4 || myDataSet != null && i == 5) {
+                            Timestamp ts = Timestamp.valueOf(myDataSet);
+                            myDataSet = myFormat.format(ts);
+                        }
                     myValue[i-1] = myDataSet;
                 }  
                 myTableModel.addRow(myValue); 
@@ -700,7 +728,8 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
                         JOptionPane.ERROR_MESSAGE);
                     return;
                 }
-                if (DataSet_Mode.equals("edit") && !Old_Bezeichnung.equals(jFormattedTextField_Bezeichnung.getText().trim())) {
+                if (DataSet_Mode.equals("edit") && !Old_Bezeichnung.equals(jFormattedTextField_Bezeichnung.getText().trim())
+                        || !Old_Beschreibung.equals(jTextArea_Beschreibung.getText().trim())) {
                     do_updateDataSet_inDB();
                 }
                 if (DataSet_Mode.equals("duplicate")) {
@@ -830,17 +859,23 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
                 jFormattedTextField_Bezeichnung.setText(tempString);
                 }
                 else {jFormattedTextField_Bezeichnung.setText("");}
-                                
+                
                 if (myTableModel.getValueAt(myRow, 2) != null) {
-                    jTextField_Anlagedatum.setText(myTableModel.getValueAt(myRow, 2).toString().trim());
+                tempString = myTableModel.getValueAt(myRow, 2).toString();
+                jTextArea_Beschreibung.setText(tempString);
+                }
+                else {jTextArea_Beschreibung.setText("");}
+                                                
+                if (myTableModel.getValueAt(myRow, 3) != null) {
+                    jTextField_Anlagedatum.setText(myTableModel.getValueAt(myRow, 3).toString().trim());
                 }
                 else {jTextField_Anlagedatum.setText("");}
-                if (myTableModel.getValueAt(myRow, 3) != null) {
-                   jTextField_Änderungsdatum.setText(myTableModel.getValueAt(myRow, 3).toString().trim());
+                if (myTableModel.getValueAt(myRow, 4) != null) {
+                   jTextField_Änderungsdatum.setText(myTableModel.getValueAt(myRow, 4).toString().trim());
                 }
                 else {jTextField_Änderungsdatum.setText("");}
-                if (myTableModel.getValueAt(myRow, 4) != null) {
-                    jTextField_Benutzer.setText(myTableModel.getValueAt(myRow, 4).toString().trim()); 
+                if (myTableModel.getValueAt(myRow, 5) != null) {
+                    jTextField_Benutzer.setText(myTableModel.getValueAt(myRow, 5).toString().trim()); 
                 }
                 else {jTextField_Benutzer.setText("");}                
                 
@@ -858,6 +893,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     private void set_textFieldsEmpty() {
         jTextField_key.setText("");
         jFormattedTextField_Bezeichnung.setText("");
+        jTextArea_Beschreibung.setText("");
         jTextField_Anlagedatum.setText("");
         jTextField_Änderungsdatum.setText("");
         jTextField_Benutzer.setText("");
@@ -865,6 +901,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     private void set_oldValues() {        
         Old_Key = jTextField_key.getText().trim();
         Old_Bezeichnung = jFormattedTextField_Bezeichnung.getText().trim();
+        Old_Beschreibung = jTextArea_Beschreibung.getText().trim();
         Old_Anlagedatum = jTextField_Anlagedatum.getText();
         Old_Änderungsdatum = jTextField_Änderungsdatum.getText();
         Old_Benutzer = jTextField_Benutzer.getText();
@@ -872,6 +909,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     private void get_oldValues() {         
         jTextField_key.setText(Old_Key);
         jFormattedTextField_Bezeichnung.setText(Old_Bezeichnung);
+        jTextArea_Beschreibung.setText(Old_Beschreibung);
         jTextField_Anlagedatum.setText(Old_Anlagedatum);
         jTextField_Änderungsdatum.setText(Old_Änderungsdatum);
         jTextField_Benutzer.setText(Old_Benutzer);
@@ -883,10 +921,12 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
         jTextField_key.setEnabled(aBoolean);
         
         jFormattedTextField_Bezeichnung.setEnabled(true);
+        jTextArea_Beschreibung.setEnabled(true);
     }
     private void set_textFieldsDisabled() {        
         jTextField_key.setEnabled(false);
         jFormattedTextField_Bezeichnung.setEnabled(false);
+        jTextArea_Beschreibung.setEnabled(false);
     }
     private void do_insertDataSet_intoDB() {        
         try
@@ -902,9 +942,10 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
             String result = myFormat.format(Anlagedatum);
             Benutzer = System.getProperty("user.name"); 
             Statement myStatement = myConnection.createStatement();
-            myStatement.executeUpdate("INSERT INTO DiafBDE.dbo.T_Ausbrechstempel (pKey_ASB, Bezeichnung, Anlagedatum, Benutzer)" 
+            myStatement.executeUpdate("INSERT INTO DiafBDE.dbo.T_Ausbrechstempel (pKey_ASB, Bezeichnung, Beschreibung, Anlagedatum, Benutzer)" 
                     + "VALUES ('" + jTextField_key.getText().trim() + "', '" 
-                    + jFormattedTextField_Bezeichnung.getText().trim() + "', '" 
+                    + jFormattedTextField_Bezeichnung.getText().trim() + "', '"
+                    + jTextArea_Beschreibung.getText().trim() + "', '" 
                     + result + "', '" 
                     + Benutzer +"')");              
             } 
@@ -936,6 +977,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
             Benutzer = System.getProperty("user.name"); 
             Statement myStatement = myConnection.createStatement();
             myStatement.executeUpdate("UPDATE DiafBDE.dbo.T_Ausbrechstempel SET Bezeichnung = '" + jFormattedTextField_Bezeichnung.getText().trim() +
+                    "', Beschreibung = '" + jTextArea_Beschreibung.getText().trim() +
                     "', Änderungsdatum = '" + result +
                     "', Benutzer = '" + Benutzer +
                     
@@ -1047,8 +1089,10 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel_editTextFields;
     private javax.swing.JPanel jPanel_footer;
     private javax.swing.JPanel jPanel_table;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane_dbData;
     private javax.swing.JTable jTable_dbData;
+    private javax.swing.JTextArea jTextArea_Beschreibung;
     private javax.swing.JTextField jTextField_Anlagedatum;
     private javax.swing.JTextField jTextField_Benutzer;
     private javax.swing.JTextField jTextField_key;
@@ -1056,6 +1100,7 @@ public class Frame_DataMaintenance_Ausbrechstempel extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField_Änderungsdatum;
     private javax.swing.JLabel lbl_Anlagedatum;
     private javax.swing.JLabel lbl_Benutzer;
+    private javax.swing.JLabel lbl_Beschreibung;
     private javax.swing.JLabel lbl_Bezeichnung;
     private javax.swing.JLabel lbl_key;
     private javax.swing.JLabel lbl_rowCount;
